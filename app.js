67387c60d33823fc40eaa40b5b0534a1adc599cc
Santiago Maldini <santimaldini@gmail.com>
2021-01-14 23:50:56 -0300

    Added first colouring and formatting request

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const app = express();
 
 app.listen(3000);
 
-app.use(express.static(path.resolve(__dirname, "public")));
+const staticFolder = path.resolve(__dirname, "./public");
+app.use(express.static(staticFolder));
 
-app.get("/",(req, res) => {
-    res.sendFile(path.resolve(__dirname, "views/home.html"))
-})
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
