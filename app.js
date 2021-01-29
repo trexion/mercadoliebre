f823fb4dac6ccace0236a5fe80cc0ddc64f69915
Santiago Maldini <santimaldini@gmail.com>
2021-01-29 20:11:47 -0300

    updated for heroku deploy

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require("express");
 const path = require("path")
 const app = express();
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000, function (){
+    console.log("Server running in port 3000");
+});
 
 const staticFolder = path.resolve(__dirname, "./public");
 app.use(express.static(staticFolder));
